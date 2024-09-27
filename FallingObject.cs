bc9bbb6632e9da2cbf83a7fc045c1df23ed9d461
k-min9 <mingu4969@gmail.com>
2024-09-27 23:52:13 +0900

    Feature: add animator

--- FallingObject.cs
+++ FallingObject.cs
@@ -1,40 +1,76 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class FallingObject : MonoBehaviour
 {
-    public float fallSpeed = 800f; // 떨어지는 속도
+    public float fallSpeed = 800f;
     private RectTransform rectTransform;
     private float bottomBoundary;
+    public List<RectTransform> otherObjects;
+    private Animator animator;
 
     void Start()
     {
-        // RectTransform 컴포넌트를 가져옵니다.
         rectTransform = GetComponent<RectTransform>();
+        animator = GetComponent<Animator>();
 
-        // 캔버스 하단 경계선을 설정합니다.
         Canvas canvas = GetComponentInParent<Canvas>();
         if (canvas != null)
         {
-            // 캔버스의 하단 경계선 (y 좌표)을 구합니다.
-            bottomBoundary = -(canvas.GetComponent<RectTransform>().rect.height / 2) + rectTransform.rect.height * 1.0f;
+            // 작업 표시줄의 위치를 고려한 하단 경계 계산
+            // Rect taskbarRect = TaskbarInfo.GetTaskbarRect();
+            // float taskbarHeight = taskbarRect.height;
+            bottomBoundary = -(canvas.GetComponent<RectTransform>().rect.height / 2) + rectTransform.rect.height * 0.5f; // + taskbarHeight;
         }
+
+        // animator.SetBool("IsFalling", isFalling);
     }
 
     void Update()
     {
-        // 오브젝트가 떨어지도록 y 위치를 조정합니다.
-        Vector2 newPosition = rectTransform.anchoredPosition;
-        newPosition.y -= fallSpeed * Time.deltaTime;
+        if (!IsCollidingWithOtherObjects(rectTransform.anchoredPosition) && !StatusManager.Instance.IsFalling)
+        {
+            StartFalling();
+        }
 
-        // 캔버스 하단 경계선에 도달했는지 확인합니다.
-        if (newPosition.y > bottomBoundary)
+        if (StatusManager.Instance.IsFalling && !StatusManager.Instance.IsPicking)
         {
-            rectTransform.anchoredPosition = newPosition;
+            Vector2 newPosition = rectTransform.anchoredPosition;
+            newPosition.y -= fallSpeed * Time.deltaTime;
+
+            if (newPosition.y > bottomBoundary && !IsCollidingWithOtherObjects(newPosition))
+            {
+                rectTransform.anchoredPosition = newPosition;
+            }
+            else
+            {
+                rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, Mathf.Max(bottomBoundary, rectTransform.anchoredPosition.y));
+                StopFalling();
+            }
         }
-        else
+    }
+
+    private bool IsCollidingWithOtherObjects(Vector2 newPosition)
+    {
+        foreach (RectTransform other in otherObjects)
         {
-            // 하단 경계선에 도달하면 위치를 정지시킵니다.
-            rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, bottomBoundary);
+            if (other != rectTransform && RectTransformUtility.RectangleContainsScreenPoint(other, newPosition))
+            {
+                return true;
+            }
         }
+        return false;
+    }
+
+    public void StartFalling()
+    {
+        StatusManager.Instance.IsFalling = true;
+        // animator.SetBool("IsFalling", true);
+    }
+
+    public void StopFalling()
+    {
+        StatusManager.Instance.IsFalling = false;
+        // animator.SetBool("IsFalling", false);
     }
 }
