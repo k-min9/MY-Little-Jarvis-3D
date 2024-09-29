b5d6e58861910fbd8a853ba57ef1d7942193fc17
k-min9 <mingu4969@gmail.com>
2024-09-29 23:31:22 +0900

    Feature: StatusManager SetStatusTrueForSecond

--- ClickHandler.cs
+++ ClickHandler.cs
@@ -20,19 +20,36 @@ public class ClickHandler : MonoBehaviour, IPointerClickHandler
         
         // 좌클릭
         if (eventData.button == PointerEventData.InputButton.Left) {
-            AskBalloonManager.Instance.ShowAskBalloon();
+            // AskBalloonManager.Instance.ShowAskBalloon();
+
+            // TODO : AI 안켜져있으면 분리
+
+            StatusManager.Instance.SetStatusTrueForSecond(value => StatusManager.Instance.IsOptioning = value, 5f); // 3초간 isOptioning을 true로
+            int randomIndex = UnityEngine.Random.Range(1, 3);  // 1~2(3-1)
+            _animator.SetTrigger("doRandomMotion" + randomIndex);
+
+            // 대사 기능 잠시 비활성화
+            // Dialogue select = DialogueManager.instance.GetRandomSelect();
+            // VoiceManager.Instance.PlayAudioFromPath(select.filePath);  // 음성 재생
+            // AnswerBalloonManager.Instance.ShowAnswerBalloon();
+            // AnswerBalloonManager.Instance.ModifyAnswerBalloonText(select.japaneseDialogue);
+            // AnswerBalloonManager.Instance.HideAnswerBalloonAfterAudio();
         }
 
         // 중앙클릭
         if (eventData.button == PointerEventData.InputButton.Middle) {
+            StatusManager.Instance.SetStatusTrueForSecond(value => StatusManager.Instance.IsOptioning = value, 5f); // 3초간 isOptioning을 true로
+
             int prob = UnityEngine.Random.Range(1, 101);  // 1~100(3-1)
             // 70% 확률로 idle
             if (prob <= 70) {
                 Dialogue idle = DialogueManager.instance.GetRandomIdle();
                 VoiceManager.Instance.PlayAudioFromPath(idle.filePath);  // 음성 재생
-                AnswerBalloonManager.Instance.ShowAnswerBalloon();
-                AnswerBalloonManager.Instance.ModifyAnswerBalloonText(idle.japaneseDialogue);
-                AnswerBalloonManager.Instance.HideAnswerBalloonAfterAudio();
+                Debug.Log("idle.filePath:" + idle.filePath);
+
+                AnswerBalloonSimpleManager.Instance.ShowAnswerBalloonSimple();
+                AnswerBalloonSimpleManager.Instance.ModifyAnswerBalloonSimpleText(idle.japaneseDialogue);
+                AnswerBalloonSimpleManager.Instance.HideAnswerBalloonSimpleAfterAudio();
             // 30% 확률로 select (모션 두개 뿐)
             } else {
                 int randomIndex = UnityEngine.Random.Range(1, 3);  // 1~2(3-1)
@@ -40,11 +57,12 @@ public class ClickHandler : MonoBehaviour, IPointerClickHandler
 
                 Dialogue select = DialogueManager.instance.GetRandomSelect();
                 VoiceManager.Instance.PlayAudioFromPath(select.filePath);  // 음성 재생
-                AnswerBalloonManager.Instance.ShowAnswerBalloon();
-                AnswerBalloonManager.Instance.ModifyAnswerBalloonText(select.japaneseDialogue);
-                AnswerBalloonManager.Instance.HideAnswerBalloonAfterAudio();
+                Debug.Log("select.filePath:" + select.filePath);
+
+                AnswerBalloonSimpleManager.Instance.ShowAnswerBalloonSimple();
+                AnswerBalloonSimpleManager.Instance.ModifyAnswerBalloonSimpleText(select.japaneseDialogue);
+                AnswerBalloonSimpleManager.Instance.HideAnswerBalloonSimpleAfterAudio();
             }
-            // StartCoroutine(TestModifyAnswerBalloonText());  // 1초마다 ModifyAnswerBalloonText 호출하는 코루틴 시작
             return;
         }
 
