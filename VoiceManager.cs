4ace3fe2825547ae469f80faca192e006faa8b07
k-min9 <mingu4969@gmail.com>
2024-10-07 01:56:13 +0900

    Feature: Voicemanager func added

--- VoiceManager.cs
+++ VoiceManager.cs
@@ -1,12 +1,15 @@
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
 using UnityEngine.Networking;
 
 public class VoiceManager : MonoBehaviour
 {
-    [SerializeField] private AudioSource audioSource; // 오디오를 재생할 AudioSource
+    [SerializeField] public AudioSource audioSource; // 오디오를 재생할 AudioSource
     private static VoiceManager instance; // 싱글톤 인스턴스
+    private Queue<AudioClip> clipQueue = new Queue<AudioClip>(); // AudioClip을 저장하는 Queue
+    private bool isQueuePlaying = false;  // 현재 재생 여부를 추적하는 플래그
 
     private void Awake()
     {
@@ -21,18 +24,40 @@ public class VoiceManager : MonoBehaviour
             Destroy(gameObject);
             return;
         }
+    }
 
+    void Start()
+    {
         // 시작하자마자 재생
-        PlayAudioFromPath("/Voices/Mari/Mari_LogIn_1.ogg");
+        Dialogue greeting = DialogueManager.instance.GetRandomGreeting();
+        PlayAudioFromPath(greeting.filePath);
         // PlayWavFromPersistentPath();
+    }
+
+
+    private void Update()
+    {
+        // AudioSource가 재생 중이지 않고, Queue에 클립이 있으면 다음 클립을 재생
+        if (!audioSource.isPlaying && clipQueue.Count > 0)
+        {
+            PlayNextClip();
+        }
     }
 
     // 경로로부터 오디오를 로드하고 재생하는 함수
     public void PlayAudioFromPath(string audioPath)
     {
-        // string fullPath = "file://" + Application.dataPath + audioPath;  // Assets 패키지화 할 경우 사용
-        string fullPath = "file://" + Application.streamingAssetsPath  + audioPath;  // Assets>StreamingAssets 활용시 사용
-        StartCoroutine(LoadAudioOGG(fullPath));
+        try
+        {
+            // string fullPath = "file://" + Application.dataPath + audioPath;  // Assets 패키지화 할 경우 사용
+            // string fullPath = "file://" + Application.streamingAssetsPath  + audioPath;  // Assets>StreamingAssets 활용시 사용
+            string fullPath = Path.Combine(Application.streamingAssetsPath, audioPath);
+            Debug.Log("audioPath : " + fullPath);
+            StartCoroutine(LoadAudioOGG(fullPath));
+        } catch {
+            
+        }
+
     }
 
     public void PlayWavFromPersistentPath()
@@ -50,18 +75,25 @@ public class VoiceManager : MonoBehaviour
 
             if (uwr.result == UnityWebRequest.Result.ConnectionError || uwr.result == UnityWebRequest.Result.ProtocolError)
             {
-                Debug.LogError("오디오 로드 실패: " + uwr.error);
+                // Debug.LogError("오디오 로드 실패: " + uwr.error);
+                Debug.Log("오디오 로드 실패: " + uwr.error);
             }
             else
             {
                 AudioClip clip = DownloadHandlerAudioClip.GetContent(uwr); // 오디오 클립 가져오기
                 audioSource.clip = clip;
+                audioSource.volume = 1f; // 100%
+                try {
+                    audioSource.volume = SettingManager.Instance.settings.sound_volumeMaster/100;
+                } catch {
+                    Debug.Log("ogg volume change error");
+                }
                 audioSource.Play(); // 오디오 재생
             }
         }
     }
 
-        // 오디오 파일을 로드하는 코루틴. 늘어나면 변수화
+    // 오디오 파일을 로드하는 코루틴. 늘어나면 변수화
     private IEnumerator LoadAudioWav(string audioPath)
     {
         using (UnityWebRequest uwr = UnityWebRequestMultimedia.GetAudioClip(audioPath, AudioType.WAV))
@@ -76,10 +108,70 @@ public class VoiceManager : MonoBehaviour
             {
                 AudioClip clip = DownloadHandlerAudioClip.GetContent(uwr); // 오디오 클립 가져오기
                 audioSource.clip = clip;
+                audioSource.volume = 1f; // 100%
+                try {
+                    audioSource.volume = SettingManager.Instance.settings.sound_volumeMaster/100;
+                } catch {
+                    Debug.Log("wav volume change error");
+                }
                 audioSource.Play(); // 오디오 재생
             }
         }
     }
+
+
+    // 오디오 클립을 Queue에 추가하는 함수
+    public void AddToQueue(AudioClip clip)
+    {
+        clipQueue.Enqueue(clip); // 클립을 Queue에 추가
+
+        // 만약 현재 아무것도 재생 중이지 않다면, 바로 재생 시작
+        if (!isQueuePlaying)
+        {
+            PlayNextClip();
+        }
+    }
+
+    // Queue에 있는 다음 클립을 재생하는 함수
+    private void PlayNextClip()
+    {
+        if (clipQueue.Count > 0)
+        {
+            isQueuePlaying = true;
+            audioSource.clip = clipQueue.Dequeue();  // Queue에서 클립을 가져옴
+            audioSource.Play();  // AudioSource로 재생 시작
+        }
+        else
+        {
+            isQueuePlaying = false;  // Queue가 비었을 경우 재생 중지
+        }
+    }
+
+    public void LoadAudioWavToQueue()
+    {
+        string audioPath = Path.Combine(Application.persistentDataPath, "response.wav");
+        StartCoroutine(LoadAudioWavToQueueEnum(audioPath));
+    }
+
+    // WAV 파일을 로드하고 Queue에 추가하는 코루틴
+    private IEnumerator LoadAudioWavToQueueEnum(string audioPath)
+    {
+        using (UnityWebRequest uwr = UnityWebRequestMultimedia.GetAudioClip(audioPath, AudioType.WAV))
+        {
+            yield return uwr.SendWebRequest(); // 요청 전송
+
+            if (uwr.result == UnityWebRequest.Result.ConnectionError || uwr.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError("오디오 로드 실패: " + uwr.error);
+            }
+            else
+            {
+                AudioClip clip = DownloadHandlerAudioClip.GetContent(uwr); // 오디오 클립 가져오기
+                AddToQueue(clip); // 클립을 Queue에 추가
+            }
+        }
+    }
+
 
     // 오디오 정지 함수
     public void StopAudio()
@@ -99,7 +191,6 @@ public class VoiceManager : MonoBehaviour
 
         return null;
     }
-
 
     // VoiceManager 인스턴스에 접근하는 함수
     public static VoiceManager Instance
