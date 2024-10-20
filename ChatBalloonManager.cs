05a08cf3213b89608a9bf3258acb5758ebe8ec19
k-min9 <mingu4969@gmail.com>
2024-10-20 21:34:13 +0900

    Feature: Chatballoon manager

---
+++ ChatBalloonManager.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class ChatBalloonManager : MonoBehaviour
+{
+    [SerializeField] private Canvas _canvas; // chatBalloon 이미지
+    [SerializeField] private GameObject chatBalloon; // chatBalloon 이미지
+    [SerializeField] public RectTransform characterTransform; // chatBalloon이 표시될 캐릭터의 Transform
+    [SerializeField] private RectTransform chatBalloonTransform; // chatBalloon의 Transform
+    [SerializeField] public TMP_InputField inputField; 
+
+    // 싱글톤 인스턴스
+    private static ChatBalloonManager instance;
+
+    private void Awake()
+    {
+        // 싱글톤 패턴 구현
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        HideChatBalloon(); // 시작 시 chatBalloon 숨기기
+    }
+
+    // 상태 갱신 로직
+    private void Update()
+    {
+        if (StatusManager.Instance.IsChatting && chatBalloon.activeSelf)  // activeSelf : 활성화 여부
+        {
+            UpdateChatBalloonPosition();
+        }
+
+        if (StatusManager.Instance.IsPicking || StatusManager.Instance.IsAnswering)
+        {
+            HideChatBalloon();
+        }
+    }
+
+
+    // 싱글톤 인스턴스에 접근하는 속성
+    public static ChatBalloonManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<ChatBalloonManager>();
+            }
+            return instance;
+        }
+    }
+
+    // chatBalloon을 보이고 텍스트를 초기화하는 함수
+    public void ShowChatBalloon()
+    {
+        chatBalloon.SetActive(true);
+        inputField.text = string.Empty; // 텍스트 초기화
+        StatusManager.Instance.IsChatting = true; // StatusManager 상태 업데이트
+        UpdateChatBalloonPosition();  // chatBalloon 위치 조정하기
+
+        // 기존 애니메이션 중지 및 isChatting 애니메이션 
+    }
+    
+    // chatBalloon을 숨기는 함수
+    public void HideChatBalloon()
+    {
+        chatBalloon.SetActive(false);
+        StatusManager.Instance.IsChatting = false; 
+    }
+
+    // chatBalloon의 위치를 캐릭터 바로 위로 조정하는 함수
+    private void UpdateChatBalloonPosition()
+    {
+        Vector2 charPosition = characterTransform.anchoredPosition;
+        
+        // 캐릭터의 X 위치와 동일하게 설정
+        chatBalloonTransform.anchoredPosition = new Vector2(charPosition.x, charPosition.y + 270 * SettingManager.Instance.settings.char_size / 100f); // Y축 창크기 270만큼
+    }
+}
