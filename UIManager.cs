ff1c92eaee61ea1411d570b9c62ea47f641d8c4d
k-min9 <mingu4969@gmail.com>
2024-11-01 23:17:22 +0900

    Feature: show version and thanks

--- UIManager.cs
+++ UIManager.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using DevionGames.UIWidgets;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private GameObject charChange; // CharChange
+    [SerializeField] private GameObject version; // version+thanks
+    [SerializeField] private Text versionThanksContent; // version+thanks
     [SerializeField] private GameObject settings; // settings
+
+
 
     // 싱글톤 인스턴스
     private static UIManager instance;
@@ -26,8 +31,8 @@ public class UIManager : MonoBehaviour
 
         // 자체 함수로 비활성화
         charChange.SetActive(false);
+        version.SetActive(false);
         settings.SetActive(false);
-
     }
 
     // 싱글톤 인스턴스에 접근하는 속성
@@ -51,11 +56,34 @@ public class UIManager : MonoBehaviour
         
     }
 
+    // version-UIWidget의 Show 작동
+    public void ShowVersion()
+    {
+        version.SetActive(true);
+        UIWidget uIWidget = version.GetComponent<UIWidget>();
+        uIWidget.Show();
+
+        // Special Thanks 문자
+        string answerLanguage = SettingManager.Instance.settings.ui_language; // 표시 언어 초기화[ko, en, jp]
+        // 언어에 따른 텍스트 설정
+        if (answerLanguage == "ko")
+        {
+            versionThanksContent.text="이 프로그램은 무료로 사용할 수 있으며\n많은 기부자들의 후원으로 제작되고 있습니다.";
+        }
+        else if (answerLanguage == "jp")
+        {
+            versionThanksContent.text="このプログラムは無料で利用することができ、\n多くのパトロンの後援で制作されています。";
+        }
+        else
+        {
+            versionThanksContent.text="This program is FREE TO USE\nand is supported by many generous donors.";
+        }
+    }
+
     // charChange-UIWidget의 Show 작동
     public void showSettings()
     {
         UIWidget uIWidget = settings.GetComponent<UIWidget>();
         uIWidget.Show();
-        
     }
 }
