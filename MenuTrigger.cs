5a3bb3d967037ce1a18b359e2a0d3e19156a99e7
k-min9 <mingu4969@gmail.com>
2024-12-15 21:28:00 +0900

    Feature: holding menu

--- MenuTrigger.cs
+++ MenuTrigger.cs
@@ -7,15 +7,14 @@ using ContextMenu = DevionGames.UIWidgets.ContextMenu;
 using UnityEngine.EventSystems;
 using System;
 
-public class MenuTrigger : MonoBehaviour, IPointerDownHandler
+public class MenuTrigger : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     private TransparentWindow _transparentWindow;
-
     private ContextMenu m_ContextMenu;
-
     private bool itemChkFlag = false;
     private float chkTimer = 0f; // 타이머 변수 추가
-
+    private bool isLeftClickHeld = false; // 좌클릭 상태
+    private float leftClickHoldTime = 0f; // 좌클릭 누른 시간
 
     // Start is called before the first frame update
     private void Start()
@@ -36,10 +35,22 @@ public class MenuTrigger : MonoBehaviour, IPointerDownHandler
                 return;
             }
 
-            if (!m_ContextMenu.IsVisible)  // 자체 제공 함수
+            if (!m_ContextMenu.IsVisible)  // 자체제공함수
             {
                 StatusManager.Instance.IsOptioning = false;
                 itemChkFlag = false; // 한번 처리 후 flag 초기화
+            }
+        }
+
+        // 좌클릭 상태 체크
+        if (isLeftClickHeld && !StatusManager.Instance.IsDragging)
+        {
+            leftClickHoldTime += Time.deltaTime;
+            if (leftClickHoldTime >= 0.5f) // 0.5초 이상 누르면 우클릭 동작 실행
+            {
+                isLeftClickHeld = false; // 상태 초기화
+                leftClickHoldTime = 0f;
+                TriggerMenu();
             }
         }
     }
@@ -48,68 +59,88 @@ public class MenuTrigger : MonoBehaviour, IPointerDownHandler
     {
         if (eventData.button == PointerEventData.InputButton.Right)
         {
-            this.m_ContextMenu.Clear();
-
-            // 메뉴 추가
-            // m_ContextMenu.AddMenuItem("SimpleBalloonTest", delegate { 
-            //     AnswerBalloonSimpleManager.Instance.ShowAnswerBalloonSimpleInf();
-            //     AnswerBalloonSimpleManager.Instance.ModifyAnswerBalloonSimpleText("Work in Progress...");
-            // });
-            m_ContextMenu.AddMenuItem("Settings", delegate { 
-                UIManager.Instance.showSettings();  // 캐릭터 변경 UI 보이기
-            });
-            m_ContextMenu.AddMenuItem("Idle", delegate { 
-                StatusManager.Instance.IsFalling = false;
-                StatusManager.Instance.IsPicking = false;
-                StatusManager.Instance.IsOptioning = false;
-                PhysicsManager.Instance.SetIdleState();  // TODO : 캐릭터 spawn 만들 경우, 싱글톤에서 handler 까지 낮춰야 함
-            });
-            m_ContextMenu.AddMenuItem("Go Left", delegate { 
-                StatusManager.Instance.IsFalling = false;
-                StatusManager.Instance.IsPicking = false;
-                StatusManager.Instance.IsOptioning = false;
-                PhysicsManager.Instance.SetWalkLeftState();  // TODO : 캐릭터 spawn 만들 경우, 싱글톤에서 handler 까지 낮춰야 함
-            });
-            m_ContextMenu.AddMenuItem("Go Right", delegate { 
-                StatusManager.Instance.IsFalling = false;
-                StatusManager.Instance.IsPicking = false;
-                StatusManager.Instance.IsOptioning = false;
-                PhysicsManager.Instance.SetWalkRightState();  // TODO : 캐릭터 spawn 만들 경우, 싱글톤에서 handler 까지 낮춰야 함
-            });
-            m_ContextMenu.AddMenuItem("Change Char", delegate { 
-                UIManager.Instance.ShowCharChange();  // 캐릭터 변경 UI 보이기
-            });
-            m_ContextMenu.AddMenuItem("Change Clothes", delegate { 
-                CharManager.Instance.ChangeClothes();  // 캐릭터 옷 갈아입기
-            });
-            m_ContextMenu.AddMenuItem("Change Monitor", delegate { 
-                _transparentWindow.NextMonitor(); 
-            });      
-            // m_ContextMenu.AddMenuItem("Set Screenshot Area", delegate {  
-            //     ScreenshotManager sm = FindObjectOfType<ScreenshotManager>();  // 최상위 ScreenshotManager
-            //     sm.SetScreenshotArea();
-            // });
-            // m_ContextMenu.AddMenuItem("Get Screenshot", delegate { 
-            //     ScreenshotManager sm = FindObjectOfType<ScreenshotManager>();  // 최상위 ScreenshotManager
-            //     sm.SaveScreenshot(); 
-            // });
-            m_ContextMenu.AddMenuItem("Local AI Server", delegate { 
-                ServerManager.AskStartServer();
-            });
-            m_ContextMenu.AddMenuItem("Version", delegate { 
-                UIManager.Instance.ShowVersion();  // 캐릭터 변경 UI 보이기
-            });
-            m_ContextMenu.AddMenuItem("Exit", delegate { Application.Quit(); 
-            });
-
-            // 메뉴 보이기
-            this.m_ContextMenu.Show();
-
-            // StatusManager 관리 (1초 후)
-            StatusManager.Instance.IsOptioning = true;
-
-            chkTimer = 1f;
-            itemChkFlag = true;
+            TriggerMenu();
         }
+        else if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            isLeftClickHeld = true;
+            leftClickHoldTime = 0f; // 타이머 초기화
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Left)
+        {
+            isLeftClickHeld = false;
+            leftClickHoldTime = 0f; // 타이머 초기화
+        }
+    }
+
+    private void TriggerMenu()
+    {
+        this.m_ContextMenu.Clear();
+
+        // 메뉴 추가
+        m_ContextMenu.AddMenuItem("Settings", delegate { 
+            UIManager.Instance.showSettings();
+        });
+        m_ContextMenu.AddMenuItem("Idle", delegate { 
+            StatusManager.Instance.IsFalling = false;
+            StatusManager.Instance.IsPicking = false;
+            StatusManager.Instance.IsOptioning = false;
+            PhysicsManager.Instance.SetIdleState();
+        });
+        m_ContextMenu.AddMenuItem("Go Left", delegate { 
+            StatusManager.Instance.IsFalling = false;
+            StatusManager.Instance.IsPicking = false;
+            StatusManager.Instance.IsOptioning = false;
+            PhysicsManager.Instance.SetWalkLeftState();
+        });
+        m_ContextMenu.AddMenuItem("Go Right", delegate { 
+            StatusManager.Instance.IsFalling = false;
+            StatusManager.Instance.IsPicking = false;
+            StatusManager.Instance.IsOptioning = false;
+            PhysicsManager.Instance.SetWalkRightState();
+        });
+        m_ContextMenu.AddMenuItem("Change Char", delegate { 
+            UIManager.Instance.ShowCharChange();
+        });
+        m_ContextMenu.AddMenuItem("Change Clothes", delegate { 
+            CharManager.Instance.ChangeClothes();
+        });
+        m_ContextMenu.AddMenuItem("Erase Memory", delegate { 
+            MemoryManager.Instance.ResetConversationMemory();
+
+            AnswerBalloonSimpleManager.Instance.ShowAnswerBalloonSimpleInf();
+            AnswerBalloonSimpleManager.Instance.ModifyAnswerBalloonSimpleText("Memory Erased");
+        });
+        // m_ContextMenu.AddMenuItem("Change Monitor", delegate { 
+        //     _transparentWindow.NextMonitor(); 
+        // });
+        // m_ContextMenu.AddMenuItem("Set Screenshot Area", delegate {  
+        //     ScreenshotManager sm = FindObjectOfType<ScreenshotManager>();  // 최상위 ScreenshotManager
+        //     sm.SetScreenshotArea();
+        // });
+        // m_ContextMenu.AddMenuItem("Get Screenshot", delegate { 
+        //     ScreenshotManager sm = FindObjectOfType<ScreenshotManager>();  // 최상위 ScreenshotManager
+        //     sm.SaveScreenshot(); 
+        // });
+        // m_ContextMenu.AddMenuItem("Local AI Server", delegate { 
+        //     ServerManager.AskStartServer();
+        // });
+        m_ContextMenu.AddMenuItem("Version", delegate { 
+            UIManager.Instance.ShowVersion();
+        });
+        m_ContextMenu.AddMenuItem("Exit", delegate { Application.Quit(); });
+
+        // 메뉴 보이기
+        this.m_ContextMenu.Show();
+
+        // StatusManager 관리 (1초 후)
+        StatusManager.Instance.IsOptioning = true;
+
+        chkTimer = 1f;
+        itemChkFlag = true;
     }
 }
