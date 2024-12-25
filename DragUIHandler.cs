daccf0dac7079ec33da319a3756cd10e14362c77
k-min9 <mingu4969@gmail.com>
2024-12-26 00:12:17 +0900

    Feature: anchorPosition to localPosition

--- DragUIHandler.cs
+++ DragUIHandler.cs
@@ -8,7 +8,7 @@ public class DragUIHandler : MonoBehaviour, IDragHandler, IBeginDragHandler
     private Canvas _canvas;
 
     private Vector2 startMousePos;
-    private Vector2 startUIPos;
+    private Vector3 startUIPos; // Vector3로 변경
 
     void Start()
     {
@@ -17,14 +17,22 @@ public class DragUIHandler : MonoBehaviour, IDragHandler, IBeginDragHandler
 
     public void OnBeginDrag(PointerEventData eventData)
     {
-        // UI 최초 위치
         RectTransform rectTransform = transform.parent.GetComponent<RectTransform>();
-        startUIPos = rectTransform.anchoredPosition;
 
+        // UI 최초 위치 (로컬 포지션 사용)
+        startUIPos = rectTransform.localPosition;
+
+        // 마우스 위치 초기화
         var mousePos = Input.mousePosition;
         mousePos.x = Mathf.Clamp(mousePos.x, 0, Screen.width);
         mousePos.y = Mathf.Clamp(mousePos.y, 0, Screen.height);
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(_canvas.transform as RectTransform, mousePos, _canvas.worldCamera, out startMousePos);
+
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            _canvas.transform as RectTransform,
+            mousePos,
+            _canvas.worldCamera,
+            out startMousePos
+        );
     }
 
     public void OnDrag(PointerEventData eventData)
@@ -33,11 +41,16 @@ public class DragUIHandler : MonoBehaviour, IDragHandler, IBeginDragHandler
         var mousePos = Input.mousePosition;
         mousePos.x = Mathf.Clamp(mousePos.x, 0, Screen.width);
         mousePos.y = Mathf.Clamp(mousePos.y, 0, Screen.height);
-        RectTransformUtility.ScreenPointToLocalPointInRectangle(_canvas.transform as RectTransform, mousePos, _canvas.worldCamera, out Vector2 pos);
 
-        // offset 계산하여 이동
-        // Debug.Log("Offset : " + (-startMousePos.x+pos.x) + "/" + (-startMousePos.y+pos.y));
-        Vector3 newPos = new Vector3(startUIPos.x-startMousePos.x+pos.x, startUIPos.y-startMousePos.y+pos.y, 0);  // z=0
-        transform.parent.position = _canvas.transform.TransformPoint(newPos);
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(
+            _canvas.transform as RectTransform,
+            mousePos,
+            _canvas.worldCamera,
+            out Vector2 pos
+        );
+
+        // 이동 오프셋 계산 및 적용
+        Vector3 offset = pos - startMousePos;
+        transform.parent.localPosition = startUIPos + offset; // 로컬 포지션 기준 적용
     }
 }
