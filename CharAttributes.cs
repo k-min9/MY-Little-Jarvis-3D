d567a357e3f2a018909d5d28768f10bee376aca8
k-min9 <mingu4969@gmail.com>
2025-03-08 03:21:13 +0900

    Feature: new attributes

--- CharAttributes.cs
+++ CharAttributes.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class CharAttributes : MonoBehaviour
@@ -5,7 +6,18 @@ public class CharAttributes : MonoBehaviour
     public string nickname;  // API 호출시 사용할 char_name
     public string charcode;  // ch0001 같은 프로그램 내 코드. pk처럼 사용
     public string voicePath = "default";  // voicePath가 특수 경로일 경우(""여도 동일)
+    public string type = "3D";  // 3D, 2D 모델 구분
+    public bool isMain = true;  // main : 대화 가능, AI 연결 주체
+    public float initLocalScale = 0;
 
     public GameObject toggleClothes = null;  // 안경, 외투 등의 변환
     public GameObject changeClothes = null;  // 옷 자체를 변경
+
+    private void Start()
+    {
+        initLocalScale = transform.localScale.x;
+    }
+
+
+
 }
