e9b7324af47b38a170e3e5fede88abb5144e546f
k-min9 <mingu4969@gmail.com>
2025-04-04 02:03:27 +0900

    Feature: Animation Manager

--- AnimationManager.cs
+++ AnimationManager.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class AnimationManager : MonoBehaviour
 {
-
     // 싱글톤 인스턴스
     private static AnimationManager instance;
     public static AnimationManager Instance
@@ -19,28 +18,110 @@ public class AnimationManager : MonoBehaviour
 
     private int animationDanceTotalCount = 31;  // 31 - 1
 
+    public void Idle()
+    {
+        try
+        {
+            Animator _animator = CharManager.Instance.GetCurrentCharacter().GetComponent<Animator>();
+
+            _animator.Play("idle", 0, 0);  // 현재 애니메이션 중지
+            _animator.SetBool("isWalk", false);
+            _animator.SetBool("isRun", false);
+            _animator.SetBool("isPick", false);
+            _animator.SetBool("isDance", false);
+            _animator.SetBool("isPat", false);
+            _animator.SetBool("isListen", false);
+        }
+        catch
+        {
+            Debug.Log("No AnimationManager for Idle");
+        }
+    }
+
     public void Dance()
     {
-        Animator _animator = CharManager.Instance.GetCurrentCharacter().GetComponent<Animator>();
+        try
+        {
+            Animator _animator = CharManager.Instance.GetCurrentCharacter().GetComponent<Animator>();
 
-        _animator.Play("idle", 0, 0);  // 현재 애니메이션 중지
-        float randomIndex = Random.Range(0, animationDanceTotalCount);  // 0~30
-        _animator.SetFloat("BlendDance", randomIndex);
-        _animator.SetBool("isWalk", false);
-        _animator.SetBool("isRun", false);
-        _animator.SetBool("isPick", false);
-        _animator.SetBool("isDance", true);
+            _animator.Play("idle", 0, 0);  // 현재 애니메이션 중지
+            float randomIndex = Random.Range(0, animationDanceTotalCount);  // 0~30
+            _animator.SetFloat("BlendDance", randomIndex);
+            _animator.SetBool("isWalk", false);
+            _animator.SetBool("isRun", false);
+            _animator.SetBool("isPick", false);
+            _animator.SetBool("isDance", true);
+        }
+        catch
+        {
+            Debug.Log("No AnimationManager for Dance");
+        }
     }
 
-    public void Hide() {
-        Animator _animator = CharManager.Instance.GetCurrentCharacter().GetComponent<Animator>();
+    public void Listen()
+    {
+        try
+        {
+            Animator _animator = CharManager.Instance.GetCurrentCharacter().GetComponent<Animator>();
 
-        _animator.SetBool("isWalk", false);
-        _animator.SetBool("isRun", false);
-        _animator.SetBool("isPick", false);
-        _animator.SetBool("isDance", false
-        );   
-        _animator.SetTrigger("doHide");
+            _animator.SetBool("isWalk", false);
+            _animator.SetBool("isRun", false);
+            _animator.SetBool("isPick", false);
+            _animator.SetBool("isDance", false);
+            _animator.SetBool("isListen", true);
+        }
+        catch
+        {
+            Debug.Log("No AnimationManager for Listen");
+        }
     }
 
+    public void ListenDisable()
+    {
+        try
+        {
+            Animator _animator = CharManager.Instance.GetCurrentCharacter().GetComponent<Animator>();
+            _animator.SetBool("isListen", false);
+        }
+        catch
+        {
+            Debug.Log("No AnimationManager for ListenDisable");
+        }
+    }
+
+    public void Hide()
+    {
+        try
+        {
+            Animator _animator = CharManager.Instance.GetCurrentCharacter().GetComponent<Animator>();
+
+            _animator.SetBool("isWalk", false);
+            _animator.SetBool("isRun", false);
+            _animator.SetBool("isPick", false);
+            _animator.SetBool("isDance", false);
+            _animator.SetTrigger("doHide");
+        }
+        catch
+        {
+            Debug.Log("No AnimationManager for Hide");
+        }
+    }
+
+    public void Show()
+    {
+        try
+        {
+            Animator _animator = CharManager.Instance.GetCurrentCharacter().GetComponent<Animator>();
+
+            _animator.SetBool("isWalk", false);
+            _animator.SetBool("isRun", false);
+            _animator.SetBool("isPick", false);
+            _animator.SetBool("isDance", false);
+            _animator.SetTrigger("doShow");
+        }
+        catch
+        {
+            Debug.Log("No AnimationManager for Show");
+        }
+    }
 }
