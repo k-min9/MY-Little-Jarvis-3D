b51ec430dbe96448a5e7a68fb49ffff7ade7e238
k-min9 <mingu4969@gmail.com>
2025-08-07 03:10:29 +0900

    Feature: UI Manager for position

---
+++ Unity/UIPositionManager.cs
@@ -0,0 +1,103 @@
+using UnityEngine;
+
+public class UIPositionManager : MonoBehaviour
+{
+    private static UIPositionManager instance;
+
+    public static UIPositionManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<UIPositionManager>();
+            }
+            return instance;
+        }
+    }
+
+    private Canvas canvas;
+    private RectTransform canvasRect;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            canvas = FindObjectOfType<Canvas>();
+            canvasRect = canvas.GetComponent<RectTransform>();
+        }
+    }
+
+    // 캔버스 중앙 위치 반환
+    public Vector3 GetCanvasPositionCenter()
+    {
+        return canvas.transform.TransformPoint(Vector3.zero);
+    }
+
+    // 캔버스 왼쪽 중앙 위치 (X - Width/2)
+    public Vector3 GetCanvasPositionLeft()
+    {
+        return canvas.transform.TransformPoint(new Vector3(-canvasRect.rect.width / 2 + 200f, 0f, 0f));
+    }
+
+    // 캔버스 오른쪽 중앙 위치 (X + Width/2)
+    public Vector3 GetCanvasPositionRight()
+    {
+        return canvas.transform.TransformPoint(new Vector3(canvasRect.rect.width / 2 - 200f, 0f, 0f));
+    }
+
+    // 캔버스 상단 중앙 위치
+    public Vector3 GetCanvasPositionTop()
+    {
+        return canvas.transform.TransformPoint(new Vector3(0f, canvasRect.rect.height / 2 - 100f, 0f));
+    }
+
+    // 캔버스 하단 중앙 위치
+    public Vector3 GetCanvasPositionBottom()
+    {
+        return canvas.transform.TransformPoint(new Vector3(0f, -canvasRect.rect.height / 2 + 100f, 0f));
+    }
+
+    // 캔버스 좌상단
+    public Vector3 GetCanvasPositionTopLeft()
+    {
+        return canvas.transform.TransformPoint(new Vector3(-canvasRect.rect.width / 2 + 100f, canvasRect.rect.height / 2 - 100f, 0f));
+    }
+
+    // 캔버스 우상단
+    public Vector3 GetCanvasPositionTopRight()
+    {
+        return canvas.transform.TransformPoint(new Vector3(canvasRect.rect.width / 2 - 100f, canvasRect.rect.height / 2 - 100f, 0f));
+    }
+
+    // 캔버스 좌하단
+    public Vector3 GetCanvasPositionBottomLeft()
+    {
+        return canvas.transform.TransformPoint(new Vector3(-canvasRect.rect.width / 2 + 100f, -canvasRect.rect.height / 2 + 100f, 0f));
+    }
+
+    // 캔버스 우하단
+    public Vector3 GetCanvasPositionBottomRight()
+    {
+        return canvas.transform.TransformPoint(new Vector3(canvasRect.rect.width / 2 - 100f, -canvasRect.rect.height / 2 + 100f, 0f));
+    }
+
+    // 특정 메뉴 이름에 따라 하드코딩된 위치 반환 (예시만)
+    public Vector3 GetMenuPosition(string menuName)  
+    {
+        switch (menuName.ToLower())
+        {
+            case "guideline":
+                return canvas.transform.TransformPoint(new Vector3(0f, 100f, 0f)); // 중앙보다 위
+            case "charchange":
+                return canvas.transform.TransformPoint(new Vector3(-200f, 50f, 0f));
+            case "settings":
+                return canvas.transform.TransformPoint(new Vector3(250f, -50f, 0f));
+            case "version":
+                return canvas.transform.TransformPoint(new Vector3(0f, -200f, 0f));
+            default:
+                return GetCanvasPositionCenter(); // 기본값은 중앙
+        }
+    }
+}
