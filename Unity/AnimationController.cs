e3337a7a1f3fc9ae13e96da7ebeb9f14855aa1f3
k-min9 <mingu4969@gmail.com>
2026-05-22 20:47:03 +0900

    Fix: operator z

--- Unity/AnimationController.cs
+++ Unity/AnimationController.cs
@@ -6,6 +6,7 @@ public class AnimationController : MonoBehaviour
 {
     [SerializeField] public Animator _animator;
     private RectTransform rectTransform;
+    private CharAttributes charAttributes;  // 같은 레벨에 있음
 
     float prevNormalizedTime;
     
@@ -18,6 +19,7 @@ public class AnimationController : MonoBehaviour
     {
         _animator = GetComponent<Animator>();
         rectTransform = GetComponent<RectTransform>();
+        charAttributes = GetComponent<CharAttributes>();
         prevNormalizedTime = 0;
         IdleCount = 0; // 초기화
     }
@@ -26,21 +28,29 @@ public class AnimationController : MonoBehaviour
     {
         if (_animator.GetCurrentAnimatorStateInfo(0).IsName("idle"))
         {
+            // idle일 경우 주기적 Z축 초기화
+            // Debug.Log("Z축 초기화!");
+            Vector3 pos3D = rectTransform.anchoredPosition3D;
+            // 위치에 따라 Z 다름
+            if (charAttributes.type == "3D")
+            {
+                pos3D.z = -70.0f;  // 캐릭터
+            }
+            else
+            {
+                pos3D.z = 0.0f;  // 오퍼레이터
+            }
+
+            rectTransform.anchoredPosition3D = pos3D;
+
+            // idle 애니메이션 idleCount 횟수 실행될 경우, 랜덤 blendidle 로 변경
             float currentTime = Mathf.Repeat(_animator.GetCurrentAnimatorStateInfo(0).normalizedTime, 1);
-            
             if (currentTime < prevNormalizedTime) // 1.0 -> 0.0으로 넘어가는 순간
             {
-                // Z축 초기화
-                // Debug.Log("Z축 초기화!");
-                Vector3 pos3D = rectTransform.anchoredPosition3D;
-                pos3D.z = 0f;
-                rectTransform.anchoredPosition3D = pos3D;
-
-
                 IdleCount++; // IdleCount 증가
                 // Debug.Log("IdleCount: " + IdleCount);
 
-                if (IdleCount >= 10) // IdleCount가 10 이상일 때 실행
+                if (IdleCount >= 5) // IdleCount가 5 이상일 때 실행
                 {
                     float randomIndex = Random.Range(0, animationTotalCount);  // 0~4.99
                     _animator.SetFloat("BlendIdle", randomIndex);
