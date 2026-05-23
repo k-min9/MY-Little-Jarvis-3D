d9890b59e16026a421eb730ea9a91ae2061fc748
k-min9 <mingu4969@gmail.com>
2026-05-23 21:12:23 +0900

    Feature: new model data reg

--- Unity/ModelDataLocal.cs
+++ Unity/ModelDataLocal.cs
@@ -5,42 +5,66 @@ public static class ModelDataLocal
     // 모델 이름과 고유 ID (표시용 텍스트, 내부값)
     public static readonly List<ModelOption> ModelOptions = new List<ModelOption>
     {     
+        // new ModelOption("qwen3.6-27b", "Qwen3.6(27B)", new List<ModelFileInfo> {
+        //     new ModelFileInfo("Qwen3.6-27B-UD-IQ3_XXS.gguf", "https://huggingface.co/unsloth/Qwen3.6-27B-GGUF/resolve/main/Qwen3.6-27B-UD-IQ3_XXS.gguf?download=true", "12.4GB", 9348464640L),
+        //     new ModelFileInfo("mmproj-Qwen3.6-BF16.gguf", "https://huggingface.co/unsloth/Qwen3.6-27B-GGUF/resolve/main/mmproj-BF16.gguf?download=true", "922MB", 788529152L)
+        // }, "9.46GB"),
+        // new ModelOption("qwen3.6-27b-UC", "Qwen3.6(27B-UC)", new List<ModelFileInfo> {
+        //     new ModelFileInfo("Qwen3.6-27B-Uncensored-IQ3_XXS.gguf", "https://huggingface.co/unsloth/Qwen3.6-27B-GGUF/resolve/main/Qwen3.6-27B-Uncensored-IQ3_XXS.gguf?download=true", "12.4GB", 9348464640L),
+        //     new ModelFileInfo("mmproj-Qwen3.6-BF16.gguf", "https://huggingface.co/unsloth/Qwen3.6-27B-GGUF/resolve/main/mmproj-BF16.gguf?download=true", "922MB", 788529152L)
+        // }, "9.46GB"),
         new ModelOption("qwen3.5-9b", "Qwen3.5(9B)", new List<ModelFileInfo> {
             new ModelFileInfo("Qwen3.5-9B-Q4_K_M.gguf", "https://huggingface.co/unsloth/Qwen3.5-9B-GGUF/resolve/main/Qwen3.5-9B-Q4_K_M.gguf?download=true", "5.68GB", 9348464640L),
             new ModelFileInfo("mmproj-Qwen3.5-BF16.gguf", "https://huggingface.co/unsloth/Qwen3.5-9B-GGUF/resolve/main/mmproj-BF16.gguf?download=true", "922MB", 788529152L)
         }, "9.46GB"),
         new ModelOption("qwen3.5-9b-UC", "Qwen3.5(9B-UC)", new List<ModelFileInfo> {
-            new ModelFileInfo("Qwen3.5-9B-Uncensored-HauhauCS-Aggressive-Q4_K_M.gguf", "https://huggingface.co/Qwen/Qwen3-VL-8B-Instruct-GGUF/resolve/main/Qwen3VL-8B-Instruct-Q4_K_M.gguf?download=true", "8.71GB", 9348464640L),
-            new ModelFileInfo("mmproj-Qwen3.5-9B-Uncensored-HauhauCS-Aggressive-BF16.gguf", "https://huggingface.co/Qwen/Qwen3-VL-8B-Instruct-GGUF/resolve/main/mmproj-Qwen3VL-8B-Instruct-Q8_0.gguf?download=true", "752MB", 788529152L)
+            new ModelFileInfo("Qwen3.5-9B-Uncensored-HauhauCS-Aggressive-Q4_K_M.gguf", "https://huggingface.co/HauhauCS/Qwen3.5-9B-Uncensored-HauhauCS-Aggressive/resolve/main/Qwen3.5-9B-Uncensored-HauhauCS-Aggressive-Q4_K_M.gguf?download=true", "5.63GB", 9348464640L),
+            new ModelFileInfo("mmproj-Qwen3.5-9B-Uncensored-HauhauCS-Aggressive-BF16.gguf", "https://huggingface.co/HauhauCS/Qwen3.5-9B-Uncensored-HauhauCS-Aggressive/resolve/main/mmproj-Qwen3.5-9B-Uncensored-HauhauCS-Aggressive-BF16.gguf?download=true", "922MB", 788529152L)
+        }, "9.46GB"),
+        new ModelOption("qwen3.6-35b-A3B", "Qwen3.6(35B-A3B)", new List<ModelFileInfo> {
+            new ModelFileInfo("Qwen3.6-35B-A3B-UD-Q4_K_M.gguf", "https://huggingface.co/unsloth/Qwen3.6-35B-A3B-GGUF/resolve/main/Qwen3.6-35B-A3B-UD-Q4_K_M.gguf?download=true", "22.13GB", 23752005632L),
+            new ModelFileInfo("mmproj-Qwen3.6-BF16.gguf", "https://huggingface.co/unsloth/Qwen3.6-35B-A3B-GGUF/resolve/main/mmproj-BF16.gguf?download=true", "903MB", 788529152L)
+        }, "9.46GB"),
+        new ModelOption("qwen3.6-35b-A3B-UC", "Qwen3.6(35B-A3B-UC)", new List<ModelFileInfo> {
+            new ModelFileInfo("Qwen3.6-35B-A3B-Uncensored-HauhauCS-Aggressive-Q4_K_M.gguf", "https://huggingface.co/HauhauCS/Qwen3.6-35B-A3B-Uncensored-HauhauCS-Aggressive/resolve/main/Qwen3.6-35B-A3B-Uncensored-HauhauCS-Aggressive-Q4_K_M.gguf?download=true", "21.2GB", 23752005632L),
+            new ModelFileInfo("mmproj-Qwen3.6-UC-BF16.gguf", "https://huggingface.co/HauhauCS/Qwen3.6-35B-A3B-Uncensored-HauhauCS-Aggressive/resolve/main/mmproj-Qwen3.6-35B-A3B-Uncensored-HauhauCS-Aggressive-f16.gguf?download=true", "903MB", 788529152L)
         }, "9.46GB"),
         new ModelOption("qwen3.6-27b", "Qwen3.6(27B)", new List<ModelFileInfo> {
-            new ModelFileInfo("Qwen3.6-27B-UD-IQ3_XXS.gguf", "https://huggingface.co/unsloth/Qwen3.6-27B-GGUF/blob/main/Qwen3.6-27B-Q3_K_S.gguf?download=true", "12.4GB", 9348464640L),
-            new ModelFileInfo("mmproj-Qwen3.6-BF16.gguf", "https://huggingface.co/unsloth/Qwen3.6-27B-GGUF/resolve/main/mmproj-BF16.gguf?download=true", "922MB", 788529152L)
+            new ModelFileInfo("Qwen3.6-27B-UD-IQ3_XXS.gguf", "https://huggingface.co/unsloth/Qwen3.6-27B-GGUF/resolve/main/Qwen3.6-27B-UD-IQ3_XXS.gguf?download=true", "22.13GB", 23752005632L),
+            new ModelFileInfo("mmproj-Qwen3.6D-BF16.gguf", "https://huggingface.co/unsloth/Qwen3.6-27B-GGUF/resolve/main/mmproj-BF16.gguf?download=true", "931MB", 788529152L)
         }, "9.46GB"),
-        new ModelOption("Gemma4-26b", "Gemma4(26B)", new List<ModelFileInfo> {
+        new ModelOption("qwen3.6-27b-UC", "Qwen3.6(27B-UC)", new List<ModelFileInfo> {
+            new ModelFileInfo("Qwen3.6-27B-Uncensored-HauhauCS-Aggressive-IQ3_XS.gguf", "https://huggingface.co/HauhauCS/Qwen3.6-27B-Uncensored-HauhauCS-Aggressive/resolve/main/Qwen3.6-27B-Uncensored-HauhauCS-Aggressive-IQ3_XS.gguf?download=true", "12.0GB", 23752005632L),
+            new ModelFileInfo("mmproj-Qwen3.6D-UC-BF16.gguf", "https://huggingface.co/HauhauCS/Qwen3.6-27B-Uncensored-HauhauCS-Aggressive/resolve/main/mmproj-Qwen3.6-27B-Uncensored-HauhauCS-Aggressive-f16.gguf?download=true", "928MB", 788529152L)
+        }, "9.46GB"),
+        new ModelOption("Gemma4-26b-A4B", "Gemma4(26B-A4B)", new List<ModelFileInfo> {
             new ModelFileInfo("gemma-4-26B-A4B-it-UD-Q4_K_M.gguf", "https://huggingface.co/unsloth/gemma-4-26B-A4B-it-GGUF/resolve/main/gemma-4-26B-A4B-it-UD-Q4_K_M.gguf?download=true", "5.68GB", 9348464640L),
-            new ModelFileInfo("mmproj-BF16.gguf", "https://huggingface.co/unsloth/gemma-4-26B-A4B-it-GGUF/resolve/main/mmproj-BF16.gguf?download=true", "922MB", 788529152L)
+            new ModelFileInfo("mmproj-Gemma4-BF16.gguf", "https://huggingface.co/unsloth/gemma-4-26B-A4B-it-GGUF/resolve/main/mmproj-BF16.gguf?download=true", "922MB", 788529152L)
         }, "9.46GB"),
         new ModelOption("Gemma4-26b-UC", "Gemma4(26B-UC)", new List<ModelFileInfo> {
             new ModelFileInfo("supergemma4-26b-uncensored-fast-v2-Q4_K_M.gguf", "https://huggingface.co/Jiunsong/supergemma4-26b-uncensored-gguf-v2/resolve/main/supergemma4-26b-uncensored-fast-v2-Q4_K_M.gguf?download=true", "5.68GB", 9348464640L),
-            new ModelFileInfo("mmproj-BF16.gguf", "https://huggingface.co/unsloth/gemma-4-26B-A4B-it-GGUF/resolve/main/mmproj-BF16.gguf?download=true", "922MB", 788529152L)
+            new ModelFileInfo("mmproj-Gemma4-UC-BF16.gguf", "https://huggingface.co/unsloth/gemma-4-26B-A4B-it-GGUF/resolve/main/mmproj-BF16.gguf?download=true", "922MB", 788529152L)
         }, "9.46GB"),
-        new ModelOption("qwen3vl-8b", "Qwen3VL(8B)", new List<ModelFileInfo> {
-            new ModelFileInfo("Qwen3VL-8B-Instruct-Q4_K_M.gguf", "https://huggingface.co/Qwen/Qwen3-VL-8B-Instruct-GGUF/resolve/main/Qwen3VL-8B-Instruct-Q4_K_M.gguf?download=true", "8.71GB", 9348464640L),
-            new ModelFileInfo("mmproj-Qwen3VL-8B-Instruct-Q8_0.gguf", "https://huggingface.co/Qwen/Qwen3-VL-8B-Instruct-GGUF/resolve/main/mmproj-Qwen3VL-8B-Instruct-Q8_0.gguf?download=true", "752MB", 788529152L)
+        new ModelOption("Gemma4-E4B-UC", "Gemma4-E4B-UC", new List<ModelFileInfo> {
+            new ModelFileInfo("Gemma-4-E4B-Uncensored-HauhauCS-Aggressive-Q4_K_M.gguf", "https://huggingface.co/HauhauCS/Gemma-4-E4B-Uncensored-HauhauCS-Aggressive/resolve/main/Gemma-4-E4B-Uncensored-HauhauCS-Aggressive-Q4_K_M.gguf?download=true", "5.34GB", 9348464640L),
+            new ModelFileInfo("mmproj-Gemma-4-E4B-Uncensored-HauhauCS-Aggressive-f16.gguf", "https://huggingface.co/HauhauCS/Gemma-4-E4B-Uncensored-HauhauCS-Aggressive/resolve/main/mmproj-Gemma-4-E4B-Uncensored-HauhauCS-Aggressive-f16.gguf?download=true", "990MB", 788529152L)
         }, "9.46GB"),
-        new ModelOption("qwen-8b",  "Qwen3(8B)", new List<ModelFileInfo> {
-            new ModelFileInfo("Qwen3-8B-Q4_K_M.gguf", "https://huggingface.co/Qwen/Qwen3-8B-GGUF/resolve/main/Qwen3-8B-Q4_K_M.gguf", "4.68GB", 5024063488L)
-        }, "4.68GB"),
-        new ModelOption("qwen-14b", "Qwen3(14B)", new List<ModelFileInfo> {
-            new ModelFileInfo("Qwen3-14B-Q4_K_M.gguf", "https://huggingface.co/Qwen/Qwen3-14B-GGUF/resolve/main/Qwen3-14B-Q4_K_M.gguf", "8.38GB", 8999632896L)
-        }, "8.38GB"),
-        new ModelOption("qwen-32b", "Qwen3(32B)", new List<ModelFileInfo> {
-            new ModelFileInfo("Qwen3-32B-Q4_K_M.gguf", "https://huggingface.co/Qwen/Qwen3-32B-GGUF/resolve/main/Qwen3-32B-Q4_K_M.gguf", "18.40GB", 19759480832L)
-        }, "18.40GB"),
-        new ModelOption("qwen-30b-a3b-2507", "Qwen3(30B-A3B)", new List<ModelFileInfo> {
-            new ModelFileInfo("Qwen3-30B-A3B-Instruct-2507-Q4_K_M.gguf", "https://huggingface.co/lmstudio-community/Qwen3-30B-A3B-Instruct-2507-GGUF/resolve/main/Qwen3-30B-A3B-Instruct-2507-Q4_K_M.gguf", "17.20GB", 18468397056L)
-        }, "17.20GB"),
+        // new ModelOption("qwen3vl-8b", "Qwen3VL(8B)", new List<ModelFileInfo> {
+        //     new ModelFileInfo("Qwen3VL-8B-Instruct-Q4_K_M.gguf", "https://huggingface.co/Qwen/Qwen3-VL-8B-Instruct-GGUF/resolve/main/Qwen3VL-8B-Instruct-Q4_K_M.gguf?download=true", "8.71GB", 9348464640L),
+        //     new ModelFileInfo("mmproj-Qwen3VL-8B-Instruct-Q8_0.gguf", "https://huggingface.co/Qwen/Qwen3-VL-8B-Instruct-GGUF/resolve/main/mmproj-Qwen3VL-8B-Instruct-Q8_0.gguf?download=true", "752MB", 788529152L)
+        // }, "9.46GB"),
+        // new ModelOption("qwen-8b",  "Qwen3(8B)", new List<ModelFileInfo> {
+        //     new ModelFileInfo("Qwen3-8B-Q4_K_M.gguf", "https://huggingface.co/Qwen/Qwen3-8B-GGUF/resolve/main/Qwen3-8B-Q4_K_M.gguf", "4.68GB", 5024063488L)
+        // }, "4.68GB"),
+        // new ModelOption("qwen-14b", "Qwen3(14B)", new List<ModelFileInfo> {
+        //     new ModelFileInfo("Qwen3-14B-Q4_K_M.gguf", "https://huggingface.co/Qwen/Qwen3-14B-GGUF/resolve/main/Qwen3-14B-Q4_K_M.gguf", "8.38GB", 8999632896L)
+        // }, "8.38GB"),
+        // new ModelOption("qwen-32b", "Qwen3(32B)", new List<ModelFileInfo> {
+        //     new ModelFileInfo("Qwen3-32B-Q4_K_M.gguf", "https://huggingface.co/Qwen/Qwen3-32B-GGUF/resolve/main/Qwen3-32B-Q4_K_M.gguf", "18.40GB", 19759480832L)
+        // }, "18.40GB"),
+        // new ModelOption("qwen-30b-a3b-2507", "Qwen3(30B-A3B)", new List<ModelFileInfo> {
+        //     new ModelFileInfo("Qwen3-30B-A3B-Instruct-2507-Q4_K_M.gguf", "https://huggingface.co/lmstudio-community/Qwen3-30B-A3B-Instruct-2507-GGUF/resolve/main/Qwen3-30B-A3B-Instruct-2507-Q4_K_M.gguf", "17.20GB", 18468397056L)
+        // }, "17.20GB"),
 
         // 추후 ModelOption 추가
     };
